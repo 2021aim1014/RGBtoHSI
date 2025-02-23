a2de616bf173c41c908c92b8cfa02cec996964c1
Seema <seema.22csz0006@iitrpr.ac.in>
2025-02-23 13:50:07 +0530

    Update hsi_dataset.py

--- hsi_dataset.py
+++ hsi_dataset.py
@@ -3,48 +3,45 @@ import numpy as np
 import random
 import cv2
 import h5py
+import os
+import spectral
+import matplotlib.pyplot as plt
 
 class TrainDataset(Dataset):
-    def __init__(self, data_root, crop_size, arg=True, bgr2rgb=True, stride=8):
-        self.crop_size = crop_size
-        self.hypers = []
-        self.bgrs = []
-        self.arg = arg
-        h,w = 482,512  # img shape
+    def __init__(self, data_root='dataset/pear dataset/train_dataset/', crop_size=128, arg=True, bgr2rgb=True, stride=8):
+        self.crop_size = crop_size 
+        self.hypers = [] #cropped hsi data
+        self.bgrs = []   # cropped rgb data
+        self.arg = arg # augment
+        self.h, self.w = 512,512  # img shape
         self.stride = stride
-        self.patch_per_line = (w-crop_size)//stride+1
-        self.patch_per_colum = (h-crop_size)//stride+1
+        self.patch_per_line = (self.w-crop_size)//self.stride + 1
+        self.patch_per_colum = (self.h-crop_size)//self.stride + 1
         self.patch_per_img = self.patch_per_line * self.patch_per_colum
 
-        hyper_data_path = f'{data_root}/Train_Spec/'
-        bgr_data_path = f'{data_root}/Train_RGB/'
+        dataset_list = os.listdir(data_root)
 
-        with open(f'{data_root}/split_txt/train_list.txt', 'r') as fin:
-            hyper_list = [line.replace('\n','.mat') for line in fin]
-            bgr_list = [line.replace('mat','jpg') for line in hyper_list]
-        hyper_list.sort()
-        bgr_list.sort()
-        print(f'len(hyper) of ntire2022 dataset:{len(hyper_list)}')
-        print(f'len(bgr) of ntire2022 dataset:{len(bgr_list)}')
-        for i in range(len(hyper_list)):
-            hyper_path = hyper_data_path + hyper_list[i]
-            if 'mat' not in hyper_path:
+        for image_folder in dataset_list:
+            if image_folder == '.DS_Store':
                 continue
-            with h5py.File(hyper_path, 'r') as mat:
-                hyper =np.float32(np.array(mat['cube']))
-            hyper = np.transpose(hyper, [0, 2, 1])
-            bgr_path = bgr_data_path + bgr_list[i]
-            assert hyper_list[i].split('.')[0] ==bgr_list[i].split('.')[0], 'Hyper and RGB come from different scenes.'
-            bgr = cv2.imread(bgr_path)
-            if bgr2rgb:
+            file_path = data_root+image_folder
+            rgb_path = file_path + '/' + image_folder + '.png'
+            hsi_path = file_path + '/REFLECTANCE_' + image_folder + '.hdr'
+            print(image_folder, " loaded")
+
+            bgr = cv2.imread(rgb_path)
+            if True:
                 bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+                
             bgr = np.float32(bgr)
             bgr = (bgr-bgr.min())/(bgr.max()-bgr.min())
-            bgr = np.transpose(bgr, [2, 0, 1])  # [3,482,512]
-            self.hypers.append(hyper)
+            bgr = np.transpose(bgr, [2, 0, 1])  # [3,512,512]
             self.bgrs.append(bgr)
-            mat.close()
-            print(f'Ntire2022 scene {i} is loaded.')
+
+            hyper = spectral.open_image(hsi_path).load()
+            hyper = np.transpose(hyper, (2, 1, 0)) # [204, 512, 512] 
+            self.hypers.append(hyper)
+
         self.img_num = len(self.hypers)
         self.length = self.patch_per_img * self.img_num
 
@@ -69,49 +66,48 @@ class TrainDataset(Dataset):
         hyper = self.hypers[img_idx]
         bgr = bgr[:,h_idx*stride:h_idx*stride+crop_size, w_idx*stride:w_idx*stride+crop_size]
         hyper = hyper[:, h_idx * stride:h_idx * stride + crop_size,w_idx * stride:w_idx * stride + crop_size]
+        
         rotTimes = random.randint(0, 3)
         vFlip = random.randint(0, 1)
         hFlip = random.randint(0, 1)
         if self.arg:
             bgr = self.arguement(bgr, rotTimes, vFlip, hFlip)
             hyper = self.arguement(hyper, rotTimes, vFlip, hFlip)
+        
         return np.ascontiguousarray(bgr), np.ascontiguousarray(hyper)
 
     def __len__(self):
         return self.patch_per_img*self.img_num
 
 class ValidDataset(Dataset):
-    def __init__(self, data_root, bgr2rgb=True):
-        self.hypers = []
-        self.bgrs = []
-        hyper_data_path = f'{data_root}/Train_Spec/'
-        bgr_data_path = f'{data_root}/Train_RGB/'
-        with open(f'{data_root}/split_txt/valid_list.txt', 'r') as fin:
-            hyper_list = [line.replace('\n', '.mat') for line in fin]
-            bgr_list = [line.replace('mat','jpg') for line in hyper_list]
-        hyper_list.sort()
-        bgr_list.sort()
-        print(f'len(hyper_valid) of ntire2022 dataset:{len(hyper_list)}')
-        print(f'len(bgr_valid) of ntire2022 dataset:{len(bgr_list)}')
-        for i in range(len(hyper_list)):
-            hyper_path = hyper_data_path + hyper_list[i]
-            if 'mat' not in hyper_path:
+    def __init__(self, data_root='dataset/pear dataset/val_dataset/', bgr2rgb=True):
+        self.hypers = [] #cropped hsi data
+        self.bgrs = []   # cropped rgb data
+
+        dataset_list = os.listdir(data_root)
+        print(' Validation Dataset')
+
+        for image_folder in dataset_list:
+            if image_folder == '.DS_Store':
                 continue
-            with h5py.File(hyper_path, 'r') as mat:
-                hyper = np.float32(np.array(mat['cube']))
-            hyper = np.transpose(hyper, [0, 2, 1])
-            bgr_path = bgr_data_path + bgr_list[i]
-            assert hyper_list[i].split('.')[0] == bgr_list[i].split('.')[0], 'Hyper and RGB come from different scenes.'
-            bgr = cv2.imread(bgr_path)
-            if bgr2rgb:
+            file_path = data_root+image_folder
+            rgb_path = file_path + '/' + image_folder + '.png'
+            hsi_path = file_path + '/REFLECTANCE_' + image_folder + '.hdr'
+            print(image_folder, " loaded")
+
+            bgr = cv2.imread(rgb_path)
+            if True:
                 bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+                
             bgr = np.float32(bgr)
-            bgr = (bgr - bgr.min()) / (bgr.max() - bgr.min())
-            bgr = np.transpose(bgr, [2, 0, 1])
-            self.hypers.append(hyper)
+            bgr = (bgr-bgr.min())/(bgr.max()-bgr.min())
+            bgr = np.transpose(bgr, [2, 0, 1])  # [3,512,512]
             self.bgrs.append(bgr)
-            mat.close()
-            print(f'Ntire2022 scene {i} is loaded.')
+
+            hyper = spectral.open_image(hsi_path).load()
+            hyper = np.transpose(hyper, (2, 1, 0)) # [204, 512, 512] 
+            self.hypers.append(hyper)
+
 
     def __getitem__(self, idx):
         hyper = self.hypers[idx]
